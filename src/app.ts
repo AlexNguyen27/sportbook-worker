038667d63f8a5a54affae39d00587faeb25d3589
Cuong Tran <103cuong@gmail.com>
2020-12-17 19:41:50 +0700

    feat: add express server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
+import express from 'express';
+import { logger } from 'juno-js';
+
 import { executeJobs } from './jobs';
+import { config } from './components';
 
 const createApp = () => {
+  const app = express();
+  app.get('/', (req, res) => res.send('Chao xìn 🇻🇳'));
+  const PORT = config.port;
+  app.listen(PORT, () => {
+    logger.info(`⚡️[server]: Server is running at https://0.0.0.0:${PORT}`);
+  });
   executeJobs();
 };
 
